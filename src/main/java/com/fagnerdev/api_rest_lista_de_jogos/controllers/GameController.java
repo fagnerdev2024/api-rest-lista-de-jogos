e02921821b9463513346c6416be054a4b76eda62
fagnerdev2024 <fagnertavaresdemenezes2024@gmail.com>
2025-01-20 23:08:14 -0300

    repository, service, controller e dto

---
+++ src/main/java/com/fagnerdev/api_rest_lista_de_jogos/controllers/GameController.java
@@ -0,0 +1,31 @@
+package com.fagnerdev.api_rest_lista_de_jogos.controllers;
+
+import com.fagnerdev.api_rest_lista_de_jogos.dtos.GameMinDTO;
+import com.fagnerdev.api_rest_lista_de_jogos.services.GameService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/games")
+public class GameController {
+
+    @Autowired
+    private GameService gameService;
+
+    @GetMapping(value = "/{id}")
+    public GameDTO findById(@PathVariable Long id) {
+        GameDTO result = gameService.findById(id);
+        return result;
+    }
+
+    @GetMapping
+    public List<GameMinDTO> findAll() {
+        List<GameMinDTO> result = gameService.findAll();
+        return result;
+    }
+}
---
+++ src/main/java/com/fagnerdev/api_rest_lista_de_jogos/repositories/GameRepository.java
@@ -0,0 +1,20 @@
+package com.fagnerdev.api_rest_lista_de_jogos.repositories;
+
+import com.fagnerdev.api_rest_lista_de_jogos.entities.Game;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GameRepository extends JpaRepository<Game, Long> {
+
+	@Query(nativeQuery = true, value = """
+			SELECT tb_game.id, tb_game.title, tb_game.game_year AS gameYear, tb_game.img_url AS imgUrl,
+			tb_game.short_description AS shortDescription, tb_belonging.position
+			FROM tb_game
+			INNER JOIN tb_belonging ON tb_game.id = tb_belonging.game_id
+			WHERE tb_belonging.list_id = :listId
+			ORDER BY tb_belonging.position
+				""")
+	List<GameMinProjection> searchByList(Long listId);
+}
