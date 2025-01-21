569967941e897ae6869091b62760e5fda242bf3b
fagnerdev2024 <fagnertavaresdemenezes2024@gmail.com>
2025-01-20 21:37:54 -0300

    first commit

---
+++ src/main/java/com/fagnerdev/api_rest_lista_de_jogos/ApiRestListaDeJogosApplication.java
@@ -0,0 +1,13 @@
+package com.fagnerdev.api_rest_lista_de_jogos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestListaDeJogosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestListaDeJogosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fagnerdev/api_rest_lista_de_jogos/ApiRestListaDeJogosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fagnerdev.api_rest_lista_de_jogos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestListaDeJogosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
